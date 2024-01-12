0ca51cd81ce8edbb2e9fbba6a305074b620326b6
YYBer_42Kranken <liyuanying225@gmail.com>
2024-01-12 22:11:36 +0100

    db changed in docker, can't add blockchain part in db

--- configBlockchainTestApp/views.py
+++ configBlockchainTestApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import GameData
 from datetime import datetime
-from .deploy_sepo import tx_hash
+# from .deploy_sepo import tx_hash
 import time
 import random
 
